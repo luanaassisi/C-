7c910fca3d221a85df2ea7c625d36d824464c415
Luana Assis <luanaaraujodeassis@outlook.com>
2022-11-07 20:08:10 -0300

    Criação de aplicação

---
+++ app.cs
@@ -0,0 +1,15 @@
+// Faça um aplicação que receba seu nome, e-mail, senha e exiba.
+
+namespace ExempleAppConsole
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+        Console.WriteLine("Luana");
+        Console.WriteLine("luana@gmail.com");
+        Console.WriteLine("(11)93659-8547");
+        Console.readLine();
+        }
+    }
+}
